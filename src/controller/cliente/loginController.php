923e3f9af900f85460edc8176b9792293f328d0d
Lucas Lopes <lu.slopesss@gmail.com>
2025-11-21 13:23:39 -0300

    feature/BACK-loginController.php-implementado

---
+++ src/controller/cliente/loginController.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro</title>
+    <link
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css"
+    rel="stylesheet"
+    integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB"
+    crossorigin="anonymous"
+    />
+    <style>
+        p{
+            font-size: 20px;
+            color: red;
+        }
+    </style>
+</head>
+    <body>
+        <?php 
+            require_once "../../model/operacoesBD/crudCliente.php";
+
+            $bd = new crudCliente();
+            
+            //componentes de login
+            $cpf = $_POST["cpf"];
+            $senha = $_POST["senha"];
+
+            //busca cliente no bd
+            $user = $bd->readCliente($cpf);
+            $user_cpf = $user["id_cliente"];
+            $user_senha = $user["senha"];
+
+            if($user_cpf == $cpf && $user_senha == $senha){
+                echo "<script>window.location.href = ../../view/loggedInHome.html</script>";
+            }
+            else{
+                echo "<p>Nao foi possivel se conectar, verifique as informacoes e tente novamente</p>";
+            }
+            echo '<a href="../../view/index.html" class="btn btn-info">Voltar a pagina inicial</button>';
+            echo '<a href="../../view/login.html" class="btn btn-primary">Voltar a tela de login</button>';
+        ?>
+    </body>
+
+</html>
